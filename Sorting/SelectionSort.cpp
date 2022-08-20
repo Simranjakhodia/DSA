522c8364694afb7d89d3bdbe83a607908d9b1538
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2022-08-21 00:19:50 +0530

    Create SelectionSort.cpp

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+using namespace std;
+
+void selectionSort(vector<int>& arr, int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+        swap(arr[minIdx], arr[i]);
+    }
+}
+

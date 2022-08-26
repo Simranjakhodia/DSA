c4d21997c504e7e62124ba6cd770c26b65dface2
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2022-08-26 09:51:17 +0530

    Update QuickSort.cpp

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -70,3 +70,43 @@ int main()
     printArray( 0, n-1, arr );
     return 0;
 }
+
+
+/////
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int start, int end) {
+    int pivot = arr[(start + end) / 2];
+    while(start <= end) {
+        while(arr[start] < pivot) 
+            start++;
+        while(arr[end] > pivot)
+            end--;
+        if(start >= end)
+            return end;
+            
+        swap(arr[start], arr[end]);
+    }
+}
+void quickSort(int arr[], int start, int end) {
+    if(start >= end) 
+        return;
+        
+    int p = partition(arr, start, end);
+    quickSort(arr, start, p-1);
+    quickSort(arr, p+1, end);
+}
+int main() {
+    int arr[] = {9, 3, 4, 2, 1, 8};
+    int n = 6;
+    quickSort(arr, 0, n-1);
+    
+    for(int i = 0; i < n; i++) 
+        cout << arr[i]  << " ";
+    
+    return 0;
+}
